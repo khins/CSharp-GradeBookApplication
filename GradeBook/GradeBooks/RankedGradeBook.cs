6ff845f063b3b4b269ee5807312dd28c22297b6f
khins <kevin@hinshawfam.com>
2019-08-15 07:54:26 -0500

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -21,7 +21,7 @@ namespace GradeBook.GradeBooks
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if (grades[threshold - 1] < averageGrade)
+            if (grades[threshold - 1] <= averageGrade)
             {
                 return 'A';
             }
