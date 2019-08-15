1061b1f8c341db83dfd86a4d818d26b0d9875551
khins <kevin@hinshawfam.com>
2019-08-15 08:01:09 -0500

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -9,7 +9,7 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
-        }
+        }        
 
         public override char GetLetterGrade(double averageGrade)
         {
